34ab4bedd1350164c2ae18f4aed2152a1a2addca
Rajarshi Mazumder <raj1156mazumder@gmail.com>
2023-09-26 20:29:12 +0900

    Dropdown made to look much better

--- lib/screens/gameProfileWidgets/gameProfileRightSection.dart
+++ lib/screens/gameProfileWidgets/gameProfileRightSection.dart
@@ -1,3 +1,4 @@
+import 'package:bottle_ver2/sharedWidgets/utilityWidgets/customDropdown.dart';
 import 'package:flutter/material.dart';
 
 import '../../themes/themes.dart';
@@ -29,62 +30,17 @@ class _GameProfileRightSectionState extends State<GameProfileRightSection> {
                 borderRadius: BorderRadius.all(Radius.circular(20)),
                 child: Container(
                   decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [secondaryColor, bgPrimaryColor]),
-                  ),
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [secondaryColor, bgPrimaryColor])),
                   width: 800,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              border: Border.all(color: Colors.pink)),
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 20),
-                          child: DropdownButton<String>(
-                            value: 'Valorant',
-                            // The selected value
-                            onChanged: (String? newValue) {
-                              // Handle dropdown item selection here
-                              print('Selected: $newValue');
-                            },
-                            items: <String>[
-                              'Valorant',
-                              'CS 2',
-                              'Apex Legends',
-                              'Overwatch',
-                              'League of Legends',
-                            ].map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(
-                                  value,
-                                  style: TextStyle(
-                                    color: Colors
-                                        .blue, // Set the text color for dropdown items
-                                  ),
-                                ),
-                              );
-                            }).toList(),
-                            style: TextStyle(
-                              color: Colors
-                                  .red, // Set the text color for the button
-                            ),
-
-                            icon: Icon(
-                              Icons.arrow_drop_down,
-                              color: Colors.red, // Set the dropdown icon color
-                            ),
-
-                            dropdownColor: Colors
-                                .white, // Set the dropdown background color
-                          ),
-                        ),
+                        child: CustomDropdown(),
                       ),
                       Container(
                         height: 500,
---
+++ lib/sharedWidgets/utilityWidgets/customDropdown.dart
@@ -0,0 +1,68 @@
+import 'package:bottle_ver2/themes/themes.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+List<String> items = [
+  'Valorant',
+  'CS 2',
+  'Apex Legends',
+  'Overwatch',
+  'League of Legends',
+];
+
+class CustomDropdown extends StatefulWidget {
+  String? value;
+
+  CustomDropdown({super.key});
+
+  @override
+  State<CustomDropdown> createState() => _CustomDropdownState();
+}
+
+class _CustomDropdownState extends State<CustomDropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+          // border: Border.all(color: Colors.green, width: 4),
+
+          borderRadius: BorderRadius.circular(10),
+          color: bgSecondaryColor),
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+            value: widget.value,
+            isExpanded: true,
+            style: TextStyle(color: Colors.white),
+            selectedItemBuilder: (BuildContext context) => items
+                .map<Widget>((item) => Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                            padding: EdgeInsets.only(right: 16.0),
+                            child: Icon(Icons.palette, color: Colors.white)),
+                        Text(item),
+                      ],
+                    ))
+                .toList(),
+            iconSize: 20,
+            icon: Icon(CupertinoIcons.arrowtriangle_down),
+            items: items.map(buildMenuItem).toList(),
+            onChanged: (onChanged) {
+              setState(() {
+                widget.value = onChanged;
+              });
+            }),
+      ),
+    );
+  }
+}
+
+DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+      value: item,
+      child: Text(
+        item,
+        style: TextStyle(fontSize: 16),
+      ),
+    );
