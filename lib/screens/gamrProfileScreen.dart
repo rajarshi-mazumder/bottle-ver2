b7f050a22d0527f1eb06a2e1a82e7c26d3b6309d
Rajarshi Mazumder <raj1156mazumder@gmail.com>
2023-10-05 00:28:00 +0900

    Commit - layout seems to be working

--- lib/screens/gamrProfileScreen.dart
+++ lib/screens/gamrProfileScreen.dart
@@ -35,9 +35,37 @@ class _GameProfileScreenState extends State<GameProfileScreen> {
           pinned: true,
           expandedHeight: 600,
           flexibleSpace: FlexibleSpaceBar(
-            background: Image.asset(
-              "/fade5_1_bw.jpg",
-              fit: BoxFit.cover,
+            background: Stack(
+              children: [
+                Image.asset(
+                  "/fade5_1_bw.jpg",
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 50, vertical: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 200,
+                        width: 300,
+                        color:
+                            Colors.blue, // Set the color for the blue container
+                      ),
+                      Container(
+                        height: 200,
+                        width: 300,
+                        color: Colors.green,
+                        child: Image.asset(
+                          "/Reyna-1.png",
+                          width: 300,
+                          fit: BoxFit.cover,
+                        ), // Set the color for the green container
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -45,22 +73,38 @@ class _GameProfileScreenState extends State<GameProfileScreen> {
           child: Container(
             height: MediaQuery.of(context).size.height - 100,
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [bgPrimaryColor, bgSecondaryColor])),
-            child: Stack(children: [
-              Positioned(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [bgPrimaryColor, bgSecondaryColor],
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: MediaQuery.of(context).size.width * 0.17,
+                  top: 0,
+                  child: GameProfileLeftSection(),
+                ),
+                Positioned(
+                  top: 52,
+                  right: 200,
+                  child: GameProfileRightSection(),
+                ),
+                Positioned(
                   top: 50,
                   left: 0,
-                  child: Sidebar(isExpanded: isSidebarExpanded)),
-              Positioned(
+                  child: Sidebar(isExpanded: isSidebarExpanded),
+                ),
+                Positioned(
                   top: 50,
                   right: 0,
-                  child: RightSidebar(isExpanded: isSidebarExpanded)),
-            ]),
+                  child: RightSidebar(isExpanded: isSidebarExpanded),
+                ),
+              ],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
