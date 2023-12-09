1f06db9600ac24013d605dbf23ffbd41baa95aba
Rajarshi Mazumder <raj1156mazumder@gmail.com>
2023-12-09 14:24:05 +0900

    separated widgets for doubleElimTournament

---
+++ lib/tournamentFlow/views/doubleElimTournamentWidgets/roundWidget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'matchWidget.dart';
+
+class RoundWidget extends StatelessWidget {
+  final String bracketType;
+  final int roundNumber;
+  final int matchCount;
+  Map<String, dynamic> tournamentHashMap;
+
+  RoundWidget({
+    required this.bracketType,
+    required this.roundNumber,
+    required this.matchCount,
+    required this.tournamentHashMap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // height: 500,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Round $roundNumber',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          ...List.generate(
+              matchCount,
+              (index) => MatchWidget(
+                    bracketType: bracketType,
+                    roundNumber: roundNumber,
+                    matchNumber: index,
+                    tournamentHashMap: tournamentHashMap,
+                  )),
+        ],
+      ),
+    );
+  }
+}
